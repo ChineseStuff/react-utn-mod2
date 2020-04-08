8e4de92aebc77d5bff9cf948677411b90e64901e
chineseStuff <pablon.88@gmail.com>
2020-04-08 13:23:49 -0300

    TP OPT N2 Added API call for User Profiles in Home

--- src/components/common/HomePage.js
+++ src/components/common/HomePage.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import UserList from "../user/UserList";
 
 const HomePage = () => {
   return (
-    <div className='jumbotron'>
-      <h1>UTN Red Social</h1>
-      <p>Bienenido a la red Social del curso de ReactJS de la UTN</p>
-      <Link to='signup'>¿Aun no es miembro? Registrarse</Link>
-    </div>
+    <>
+      <div className='jumbotron'>
+        <h1>UTN Red Social</h1>
+        <p>Bienenido a la red Social del curso de ReactJS de la UTN</p>
+        <Link to='signup'>¿Aun no es miembro? Registrarse</Link>
+      </div>
+      <UserList />
+    </>
   );
 };
 
